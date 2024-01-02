1c0759c3cad226a6bebb4bef6a985413fe465caa
Tanzim Mokammel <mtanzim@gmail.com>
2024-01-02 12:23:13 -0700

    can create a todo now

--- main.go
+++ main.go
@@ -1,19 +1,64 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	_ "modernc.org/sqlite"
 )
 
+type NewTodo struct {
+	Name      string `json:"name"`
+}
+
+type Todo struct {
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
 func main() {
+
+	db, err := sql.Open("sqlite", "./todo.db")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, name TEXT, completed BOOLEAN)")
+	if err != nil {
+		panic(err)
+	}
 
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world"))
 	})
 	r.Post("/api/v1/todo/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Create todo"))
+		var todo Todo
+		err := json.NewDecoder(r.Body).Decode(&todo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		res, err := db.Exec("INSERT INTO todos (name, completed) VALUES (?, ?)", todo.Name, false)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		id, err := res.LastInsertId()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		insertedTodo := Todo{
+			ID:        id,
+			Name:      todo.Name,
+			Completed: todo.Completed,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(insertedTodo)
+
 	})
 	r.Put("/api/v1/todo/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
