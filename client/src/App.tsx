c607ec5f5f5eb0e30735886c6892421b77f340dd
Tanzim Mokammel <mtanzim@gmail.com>
2024-01-02 15:41:46 -0700

    client can add todo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 interface Todo {
@@ -15,13 +13,23 @@ function fetchTodos(): Promise<Todo[]> {
   return fetch(`${API_ENDPOINT}`).then((res) => res.json());
 }
 
+function addTodo(name: string): Promise<Todo> {
+  return fetch(`${API_ENDPOINT}`, {
+    method: "POST",
+    body: JSON.stringify({ name }),
+  }).then((res) => res.json());
+}
+
 function Todo() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [errMsg, setErrMsg] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+  const [newTodo, setNewTodo] = useState<string>("");
+
   useEffect(() => {
     const onMount = async () => {
       try {
+        setLoading(true);
         const todos = await fetchTodos();
         setTodos(todos);
       } catch (err) {
@@ -30,33 +38,57 @@ function Todo() {
           return;
         }
         setErrMsg("Something went wrong");
+      } finally {
+        setLoading(false);
       }
     };
     onMount();
   }, []);
 
+  async function handleAdd() {
+    if (!newTodo) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const res = await addTodo(newTodo);
+      setTodos((cur) => cur.concat(res));
+    } catch (err) {
+      if (err instanceof Error) {
+        setErrMsg(err.message);
+        return;
+      }
+      setErrMsg("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  if (errMsg) {
-    return <div>{errMsg}</div>;
-  }
-
   return (
     <div>
-      <ul>
-        {todos.map((todo) => (
-          <li>{todo.name}</li>
-        ))}
-      </ul>
+      <h1>To do list</h1>
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <div>No todos</div>
+      )}
+      <input type="text" onChange={(e) => setNewTodo(e.target.value)}></input>
+      <button onClick={handleAdd}>Add</button>
+      {errMsg ?? <p>{errMsg}</p>}
     </div>
   );
 }
 
 function App() {
-
-  return (<Todo />);
+  return <Todo />;
 }
 
 export default App;
